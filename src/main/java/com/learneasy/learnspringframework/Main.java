4f8e30a5779613cff4f5fec8d50f557b486ce5d6
Geetha <chgeethakrishna@gmail.com>
2023-06-04 05:47:42 +0530

    Creating a spring bean

--- src/main/java/com/learneasy/learnspringframework/Main.java
+++ src/main/java/com/learneasy/learnspringframework/Main.java
@@ -1,16 +1,19 @@
 package com.learneasy.learnspringframework;
 
-import com.learneasy.learnspringframework.game.DrivingGame;
-import com.learneasy.learnspringframework.game.GameRunner;
+import com.learneasy.learnspringframework.configuration.HelloWorldConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        // CricketGame game = new CricketGame();
-        // FootballGame game = new FootballGame();
-        DrivingGame game = new DrivingGame();
-        GameRunner runner = new GameRunner(game);
-        runner.run();
+        // 1. Create a configuration class and a bean
+
+        // 2. Initialize the spring context
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+        // 3. Retrieve the bean
+        System.out.println(context.getBean("name"));
+
     }
 
 }
